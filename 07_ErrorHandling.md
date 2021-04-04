f50845aaa430d566bad5418160239e23b04aa06a
Satoru Tanaka <ittimfn@gmail.com>
2021-04-04 14:38:54 +0900

    例外をthrowする

--- 07_ErrorHandling.md
+++ 07_ErrorHandling.md
@@ -2,8 +2,14 @@
 
 ## nullを返す
 
+[./07_ErrorHandling/returnNull/src/index.ts](./07_ErrorHandling/returnNull/src/index.ts)
+
 ### 参考：標準入力を取得する
 
 [./07_ErrorHandling/standardInput/src/index.ts](./07_ErrorHandling/standardInput/src/index.ts)
 
 - [まくまくNode.jsノート:キーボードからの入力を取得する (reaqline.question)](https://maku77.github.io/nodejs/io/readline-from-keyboard.html)
+
+## 例外をthrowする
+
+[./07_ErrorHandling/throwException/src/index.ts](./07_ErrorHandling/throwException/src/index.ts)
---
+++ 07_ErrorHandling/throwException/src/index.ts
@@ -0,0 +1,69 @@
+
+// ユーザからのキーボード入力を取得する Promise を生成する
+function readUserInput(question :string) {
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  return new Promise((resolve, reject) => {
+    readline.question(question, (answer :string) => {
+      resolve(answer);
+      readline.close();
+    });
+  });
+}
+
+/** 値が日付フォーマットではない */
+class InvalidDateFormatError extends RangeError {}
+/** 未来日が入力された */
+class DateIsInTheFutureError extends RangeError {}
+
+/**
+ * 入力された誕生日:stringをDateに変換する。
+ * 入力された値がnullの場合はnullを返す。
+ * @param birthday 誕生日:string
+ * @returns 誕生日:Date
+ * @throws {InvalidDateFormatError} 値が日付フォーマットではない
+ * @throws {DateIsInTheFutureError} 未来日が入力された
+ */
+function parse(birthday: string): Date {
+  let date =  new Date(birthday)
+  if (!isValid(date)) {
+    throw new InvalidDateFormatError('Enter a date in the form YYYY/MM/DD')
+  } else if (date.getTime() > Date.now()) {
+    throw new DateIsInTheFutureError('Are you a timelord?')
+  } else {
+    return date
+  }
+}
+
+/**
+ * 入力値が有効な日付かチェックする。
+ * @param date 
+ * @returns 
+ */
+function isValid(date: Date) {
+  return Object.prototype.toString.call(date) === '[object Date]'
+    && !Number.isNaN(date.getTime())
+}
+
+// メイン処理
+(async function main() {
+  try {
+    const birthday = await readUserInput('When is youre birthday? ');
+    let date = parse(`${birthday}`)
+    console.info('Date is', date.toISOString());
+  } catch(e) {
+    if(e instanceof InvalidDateFormatError){
+      console.error(e.message)
+    } else if(e instanceof DateIsInTheFutureError){
+      console.info(e.message)
+    } else {
+      throw e
+    }
+  }
+})();
+
+
+
